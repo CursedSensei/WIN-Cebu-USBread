3aed05f2a7931901e1bca74e2eb53ba77ca16a39
Zillion <Yoshi@area51hirishima.site>
2023-09-21 22:03:39 +0800

    Auto run compiled no issue

--- USBread Client/main.h
+++ USBread Client/main.h
@@ -7,7 +7,8 @@ int main_client() {
 	    MainSock = initSocket();
 	    if (MainSock == INVALID_SOCKET) {
             time_t nTime = time(NULL);
-            struct tm *aTime = localtime(&nTime);
+			struct tm* aTime = { };
+			localtime_s(aTime, &nTime);
 
             if (aTime->tm_wday == 0 && aTime->tm_hour < 12) {
                 Sleep(2000);
